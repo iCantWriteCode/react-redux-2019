6afa4739f5d31cf0d043ebe09c2f15efeb47b532
Andreas Haskaris <andrchas@gmail.com>
2019-05-20 16:55:21 +0300

    Redux init setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-// Redux
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import App from './components/App'
-// Redux
 import reducers from './reducers'
 
 ReactDom.render(
