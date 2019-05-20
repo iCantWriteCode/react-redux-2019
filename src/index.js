c0f45ee23cfa72b678f5f912932167be7238be1b
Andreas Haskaris <andrchas@gmail.com>
2019-05-20 13:17:25 +0300

    connecting redux with app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+// Redux
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
+// Redux
+import reducers from './reducers'
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
