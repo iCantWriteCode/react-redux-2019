ef58b3778961613912994ad8ce9941fbcb59162f
Andreas Haskaris <andrchas@gmail.com>
2019-05-21 14:42:15 +0300

    Connecting Component with redux store and api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,15 @@ import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
+
+/**
+|--------------------------------------------------
+| Above Method is a Refactored version of the below
+|--------------------------------------------------
+*/
+
 // export const fetchPosts = async () => {
 //     return async function(dispatch, getState) {
 //         const response = await jsonPlaceholder.get('/posts')
